a3edee93915a4469688de154f8c15e6bb430d271
Facundo Lerena <faculerena@gmail.com>
2023-05-04 22:57:56 -0300

    usuarioConMasAmigos, estaRobertoCarlos

--- iap1-tp.hs
+++ iap1-tp.hs
@@ -76,14 +76,20 @@ cantidadDeAmigos rs user = length (amigosDe rs user)
 
 -- describir qué hace la función: .....
 usuarioConMasAmigos :: RedSocial -> Usuario
-usuarioConMasAmigos = undefined
+usuarioConMasAmigos rs = encontrarUsuarioConMasAmigos rs (usuarios rs)
 
+
+
+encontrarUsuarioConMasAmigos :: RedSocial -> [Usuario] -> Usuario
+encontrarUsuarioConMasAmigos rs (x:[]) = x 
+encontrarUsuarioConMasAmigos rs (x:xs) | length (amigosDe rs x) > length (amigosDe rs (encontrarUsuarioConMasAmigos rs xs)) = x
+                                       | otherwise = encontrarUsuarioConMasAmigos rs xs
 
 
 
 -- describir qué hace la función: .....
 estaRobertoCarlos :: RedSocial -> Bool
-estaRobertoCarlos = undefined
+estaRobertoCarlos rs = cantidadDeAmigos rs (usuarioConMasAmigos rs) > 1000000 
 
 
 
@@ -168,3 +174,38 @@ usuariosB = [usuario1, usuario2, usuario3, usuario5]
 relacionesB = [relacion1_2, relacion2_3]
 publicacionesB = [publicacion1_3, publicacion1_4, publicacion1_5, publicacion3_1, publicacion3_2, publicacion3_3]
 redB = (usuariosB, relacionesB, publicacionesB)
+
+
+usuario6 = (6, "Santiago")
+usuario7 = (7, "Laura")
+usuario8 = (8, "Miguel")
+usuario9 = (9, "Florencia")
+usuario10 = (10, "José")
+usuario11 = (11, "Camila")
+usuario12 = (12, "Facundo")
+usuario13 = (13, "Valentina")
+usuario14 = (14, "Gabriel")
+usuario15 = (15, "Marina")
+
+usuariosC = [usuario1, usuario2, usuario3, usuario4, usuario5, usuario6, usuario7, usuario8, usuario9, usuario10, usuario11, usuario12, usuario13, usuario14, usuario15]
+
+relacion2_5 = (usuario5, usuario2)
+relacion3_5 = (usuario5, usuario3)
+relacion4_5 = (usuario5, usuario4)
+relacion6_7 = (usuario6, usuario7)
+relacion8_9 = (usuario8, usuario9)
+relacion10_11 = (usuario10, usuario11)
+relacion12_13 = (usuario12, usuario13)
+relacion14_15 = (usuario14, usuario15)
+relacion12_1 = (usuario12, usuario1)
+relacion12_2 = (usuario12, usuario2)
+relacion12_3 = (usuario12, usuario3)
+relacion12_4 = (usuario12, usuario4)
+relacion12_5 = (usuario12, usuario5)
+
+relacionesC = [relacion12_1, relacion12_2, relacion12_3, relacion12_4, relacion12_5, relacion1_2, relacion1_3, relacion1_4, relacion2_3, relacion2_4, relacion2_5, relacion3_4, relacion3_5, relacion4_5, relacion6_7, relacion8_9, relacion10_11, relacion12_13, relacion14_15]
+
+
+redC = (usuariosC, relacionesC, [])
+
+
